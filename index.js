13b758a0e658434fc423e9a697a58ff6d7bd2e6a
ASANAKIDENSEY <asanakidensey@gmail.com>
2023-03-18 16:59:22 +0300

    slider test 3

--- index.js
+++ index.js
@@ -1,42 +1,11 @@
-const caruselImage = document.querySelectorAll(".carusel_img"),
-  caruselLine = document.querySelector(".carusel_line"),
-  caruselDots = document.querySelector(".carusel__dot"),
-  caruselBtnNext = document.querySelector(".btn__carusel-next"),
-  caruselBtnPrev = document.querySelector(".btn__carusel-prev");
+//--Swiper
+const swiper = new Swiper(".slider-popular-box", {
+  // spacebetween: 20,
+  // slidePerView: 3,
+  // Navigation arrows
 
-let caruselCount = 0;
-let caruselWidth;
-
-//adaptability
-// window.addEventListener("resize", showCarusel);
-
-//Btn slide next and prev
-caruselBtnNext.addEventListener("click", nextCarusel);
-caruselBtnPrev.addEventListener("click", PrevCarusel);
-
-function showCarusel() {
-  caruselWidth = document.querySelector(".carusel").offsetWidth;
-  caruselLine.style.width = caruselWidth * caruselImage.length + "px";
-  caruselImage.forEach((item) => (item.style.width = caruselWidth + "px"));
-
-  rollCarusel();
-}
-
-function nextCarusel() {
-  caruselCount++;
-  if (caruselCount >= caruselImage.length) caruselCount = 0;
-
-  rollCarusel();
-}
-
-function prevCarusel() {
-  caruselCount--;
-  if (caruselCount < 0) caruselCount = caruselImage.length - 1;
-
-  rollCarusel();
-}
-
-//Roll slide
-function rollCarusel() {
-  caruselLine.style.transform = `transleteY(${-caruselCount * caruselWidth}px)`;
-}
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
