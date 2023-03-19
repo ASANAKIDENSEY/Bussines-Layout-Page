b842180ba279ee8fcaf02105cb72ccf9ca82acb6
ASANAKIDENSEY <asanakidensey@gmail.com>
2023-03-19 03:34:23 +0300

    swiper test 4

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 //--Swiper
-const swiper = new Swiper(".slider-popular-box", {
-  // spacebetween: 20,
-  // slidePerView: 3,
-  // Navigation arrows
+const swiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  // direction: "vertical",
+  // loop: true,
+  slidesPerView: 4.5,
+  spaceBetween: 10,
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+  },
 
+  // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: ".swiper-scrollbar",
+  // },
 });
