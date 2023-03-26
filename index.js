e58a437ff94f98d58c1789f2b2daf5afecd20ce0
ASANAKIDENSEY <asanakidensey@gmail.com>
2023-03-26 10:29:31 +0300

    slider block

--- index.js
+++ index.js
@@ -3,24 +3,60 @@ const swiper = new Swiper(".swiper-container", {
   // Optional parameters
   // direction: "vertical",
   loop: true,
-  slidesPerView: 4.5,
-  spaceBetween: 25,
+  slidesPerView: 1,
+  spaceBetween: 10,
   slideToClikedSlide: true,
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-    type: "bullets",
-    clickable: true,
+  autoplay: {
+    delay: 2000,
   },
+  // If we need pagination
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   type: "bullets",
+  //   clickable: true,
+  // },
 
   // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-
-  // And if we need scrollbar
-  // scrollbar: {
-  //   el: ".swiper-scrollbar",
-  // },
+  //adaptivity slider
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1.2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 1.9,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2.5,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1280: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+    1600: {
+      slidesPerView: 5,
+      spaceBetween: 20,
+    },
+    1920: {
+      slidesPerView: 6,
+      spaceBetween: 20,
+    },
+  },
 });
